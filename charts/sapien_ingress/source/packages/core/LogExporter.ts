0c0cf831caadd80297792dc8cb8b73886af9f091
Collins Hillary <collinshillary@Collinss-MacBook-Air.local>
2023-09-04 13:37:41 +0300

    Update ingress deployment-v1.6

--- charts/sapien_ingress/source/packages/core/LogExporter.ts
+++
@@ -1,7 +0,0 @@
-import { LogLevel } from "./Logger";
-
-export abstract class LogExporter
-{
-    public abstract initialize(): Promise<void>;
-    public abstract log(component: string, level: LogLevel, message: string, data: any): void;
-}
